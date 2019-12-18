8aadcb4bf721c4477105350fe55fb82a5bc218a4
mdx <martin.scorsese2019@gmail.com>
2019-12-18 11:47:17 +0200

    сom

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class MermelandoApp extends StatelessWidget {
         initialRoute: '/',
         routes: {
           '/': (context) => LoginPhone(),
-          '/code_registration': (context) => CodeRegistrationPage(),
+          '/code_registration': (context) => CodeRegistration(),
           '/list_offers': (context) => Offer(),
           '/menu': (context) => Menu(),
         },
--- lib/pages/code_registration.dart
+++ lib/pages/code_registration.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:fresa/bloc/main_model.dart';
 import 'package:fresa/common/apifunctions/requestLoginApi.dart';
 import 'package:flutter/services.dart';
 
@@ -7,13 +8,31 @@ import 'package:flutter/services.dart';
 
 //import 'package:pin_code_fields/pin_code_fields.dart';
 import 'package:pin_code_text_field/pin_code_text_field.dart';
+import 'package:flushbar/flushbar.dart';
+import 'package:scoped_model/scoped_model.dart';
 
+class CodeRegistration extends StatelessWidget {
+  String phone;
+  String status;
+  MainModel model;
+  CodeRegistration({Key key, this.phone, this.status, this.model}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ScopedModel<MainModel>(
+      model: mainModel,
+      child: ScopedModelDescendant<MainModel>(builder: (context, child, model) {
+        return CodeRegistrationPage(model: model, phone: phone, status: status,);
+      }),
+    );
+  }
+}
 
 class CodeRegistrationPage extends StatefulWidget {
   String phone;
   String status;
-
-  CodeRegistrationPage({Key key, this.phone, this.status}) : super(key: key);
+  MainModel model;
+  CodeRegistrationPage({Key key, this.phone, this.status, this.model}) : super(key: key);
 
   @override
   _CodeRegistrationPageState createState() => _CodeRegistrationPageState();
@@ -28,11 +47,58 @@ class _CodeRegistrationPageState extends State<CodeRegistrationPage> {
 
   String code = '', password = '', password2 = '';
 
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+
+  void showErrorSnackBar(String message) {
+    _scaffoldKey.currentState.showSnackBar(new SnackBar(
+      duration: Duration(milliseconds: 950),
+
+      content: Text(message),
+      backgroundColor: Colors.redAccent,)
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
 
+    Widget pinBox = PinCodeTextField(
+      autofocus: true,
+      controller: _codeController,
+      hideCharacter: false,
+      highlight: true,
+      highlightColor:  Color.fromRGBO(195, 48, 48, 1),
+      defaultBorderColor: Colors.white,
+      hasTextBorderColor: widget.model.errorCode ? Colors.redAccent : Colors.white,
+      maxLength: 4,
+      onTextChanged: (text) {
+        setState(() {
+          code = text;
+          if(text == '') {
+            widget.model.setErrorCode = false;
+          }
+        });
+      },
+      onDone: (text){
+        print("DONE $text");
+        code = text;
+        widget.model.setErrorCode = false;
+      },
+      wrapAlignment: WrapAlignment.center,
+      pinBoxOuterPadding: const EdgeInsets.symmetric(horizontal: 8.0),
+      pinBoxDecoration: ProvidedPinBoxDecoration.defaultPinBoxDecoration,
+      pinBoxHeight: 64,
+      pinBoxRadius: 8,
+      pinBoxColor: Colors.white,
+      pinBoxWidth: 50,
+      pinBoxBorderWidth: 2,
+      pinTextStyle: TextStyle(fontSize: 30.0, color:  Color.fromRGBO(87, 86, 86, 1),),
+      pinTextAnimatedSwitcherTransition: ProvidedPinBoxTextAnimation.scalingTransition,
+      pinTextAnimatedSwitcherDuration: Duration(milliseconds: 300),
+    );
+
+
+    return new Scaffold(
+        key: _scaffoldKey,
 //        resizeToAvoidBottomPadding: false,
         backgroundColor: Color.fromRGBO(254, 237, 235, 1),
 //        appBar: new AppBar(
@@ -80,36 +146,7 @@ class _CodeRegistrationPageState extends State<CodeRegistrationPage> {
 
                             Padding(
                               padding: EdgeInsets.fromLTRB(0.0, 5.0, 0.0, 10.0),
-                              child: PinCodeTextField(
-                                autofocus: true,
-                                controller: _codeController,
-                                hideCharacter: false,
-                                highlight: true,
-                                highlightColor:  Color.fromRGBO(195, 48, 48, 1),
-                                defaultBorderColor: Colors.white,
-                                hasTextBorderColor: Colors.white,
-                                maxLength: 4,
-                                onTextChanged: (text) {
-                                  setState(() {
-                                    code = text;
-                                  });
-                                },
-                                onDone: (text){
-                                  print("DONE $text");
-                                  code = text;
-                                },
-                                wrapAlignment: WrapAlignment.center,
-                                pinBoxOuterPadding: const EdgeInsets.symmetric(horizontal: 8.0),
-                                pinBoxDecoration: ProvidedPinBoxDecoration.defaultPinBoxDecoration,
-                                pinBoxHeight: 64,
-                                pinBoxRadius: 8,
-                                pinBoxColor: Colors.white,
-                                pinBoxWidth: 50,
-                                pinBoxBorderWidth: 2,
-                                pinTextStyle: TextStyle(fontSize: 30.0, color:  Color.fromRGBO(87, 86, 86, 1),),
-                                pinTextAnimatedSwitcherTransition: ProvidedPinBoxTextAnimation.scalingTransition,
-                                pinTextAnimatedSwitcherDuration: Duration(milliseconds: 300),
-                              ),
+                              child: pinBox,
                             ),
 
 
@@ -134,7 +171,9 @@ class _CodeRegistrationPageState extends State<CodeRegistrationPage> {
                                             .invokeMethod('TextInput.hide');
                                         print(code);
                                         print(_codeController.text);
-                                        requestLogIn(context, widget.phone, _codeController.text);
+
+                                        requestLogIn(context, widget.phone, _codeController.text, widget.model);
+
 
                                       }
                                     },
@@ -163,131 +202,7 @@ class _CodeRegistrationPageState extends State<CodeRegistrationPage> {
                   ],
                 ),))]))
           )
+
     );
-
-
-//        Container(
-//            padding: new EdgeInsets.all(25.0),
-//            child:   Container(child: Column(children: <Widget>[
-//              Padding(
-//                padding: EdgeInsets.fromLTRB(90.0, 70.0, 90.0, 10.0),
-//                child:  Center(
-//                  child: Image.asset('res/images/logo_m.png',),
-//                ),
-//              ),Flexible(child:  SingleChildScrollView(
-//                reverse: true,
-//                child: Column(
-////              mainAxisAlignment: MainAxisAlignment.center,
-////              crossAxisAlignment: CrossAxisAlignment.center,
-//                  children: <Widget>[
-//
-//
-//                    Form(
-//                      key: this._formKey,
-//                      child: Padding(
-//                        padding: EdgeInsets.all(0.0),
-//                        child: Column(
-//                          mainAxisSize: MainAxisSize.max,
-//                          mainAxisAlignment: MainAxisAlignment.center,
-//                          crossAxisAlignment: CrossAxisAlignment.center,
-//                          children: <Widget>[
-//
-//                            Text('Codigo de SMS', style: TextStyle(fontSize: 24,
-//                              fontWeight: FontWeight.bold,
-////                            fontFamily: 'Gilroy',
-//
-//                              color: Color.fromRGBO(74, 54, 54, 1),
-//                            ),),
-//                            Padding(
-//                              padding: EdgeInsets.fromLTRB(0.0, 5.0, 0.0, 10.0),
-//                            ),
-//                            PinCodeTextField(
-//                              autofocus: true,
-//                              controller: _codeController,
-//                              hideCharacter: false,
-//                              highlight: true,
-//                              highlightColor: Color.fromRGBO(195, 48, 48, 1),
-//                              defaultBorderColor: Colors.white,
-//                              hasTextBorderColor: Colors.redAccent,
-//                              maxLength: 4,
-//                              onTextChanged: (text) {
-//                                setState(() {
-//                                  code = text;
-//                                });
-//                              },
-//                              onDone: (text){
-//                                print("DONE $text");
-//                                code = text;
-//                              },
-//                              wrapAlignment: WrapAlignment.center,
-//                              pinBoxOuterPadding: const EdgeInsets.symmetric(horizontal: 8.0),
-//                              pinBoxDecoration: ProvidedPinBoxDecoration.defaultPinBoxDecoration,
-//                              pinBoxHeight: 64,
-//                              pinBoxRadius: 8,
-//                              pinBoxColor: Colors.white,
-//                              pinBoxWidth: 50,
-//                              pinBoxBorderWidth: 2,
-//                              pinTextStyle: TextStyle(fontSize: 30.0,color: Colors.grey),
-//                              pinTextAnimatedSwitcherTransition: ProvidedPinBoxTextAnimation.scalingTransition,
-//                              pinTextAnimatedSwitcherDuration: Duration(milliseconds: 300),
-//                            ),
-//                            Padding(
-//                              padding: EdgeInsets.fromLTRB(0.0, 5.0, 0.0, 15.0),
-//                            ),
-//
-//
-//
-//                            Padding(
-//                                padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
-//                                child: Container(
-//                                  height: 64.0,
-//                                  width: double.infinity,
-//                                  margin: const EdgeInsets.only(bottom: 1.0),
-//                                  child: RaisedButton(
-//                                    onPressed: () {
-//                                      if (_formKey.currentState.validate()) {
-//                                        _formKey.currentState.save();
-//                                        print('OK!!!!!!!');
-//                                        SystemChannels.textInput
-//                                            .invokeMethod('TextInput.hide');
-//                                        print(
-//                                            '${_codeController.text}');
-//                                        SystemChannels.textInput
-//                                            .invokeMethod('TextInput.hide');
-//                                        print(code);
-//                                        print(_codeController.text);
-//                                        requestLogIn(context, widget.phone, _codeController.text);
-//
-//                                      }
-//                                    },
-//                                    shape: RoundedRectangleBorder(
-//                                      borderRadius: new BorderRadius.circular(12.0),
-////                                  side: BorderSide(color: Colors.red)
-//                                    ),
-//                                    child: Text("SIGUIENTE PASO",
-//                                        style: TextStyle(
-//                                            fontWeight: FontWeight.w900,
-//                                            fontStyle: FontStyle.normal,
-//                                            letterSpacing: 0,
-////                                    fontFamily: 'Gilroy',
-//                                            color: Colors.white, height: 1, fontSize: 18.0
-//                                        )
-//                                    ),
-//                                    color: Color.fromRGBO(195, 48, 48, 1),
-//                                  ),
-//                                )
-//                            ),
-//
-//                          ],
-//                        ),
-//                      ),
-//                    )
-//
-//
-//                  ],
-//                ),),)
-//
-//            ],),)
-//            ,));
   }
 }
